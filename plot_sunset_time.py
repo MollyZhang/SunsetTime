4df82a6ec9c35b4eb80cb383f0785faa397edd9b
Molly Zhang <mollyzhang@ucsc.edu>
2016-02-10 15:22:20 -0800

    add final touch to plot

--- plot_sunset_time.py
+++ plot_sunset_time.py
@@ -6,6 +6,7 @@ import pickle
 from pprint import pprint as pp
 import time
 from matplotlib import pyplot as plt
+from matplotlib.dates import DateFormatter
 
 
 
@@ -14,12 +15,21 @@ DATAFILE = "sunrise_sunset_santa_cruz_2016"
 
 def main():
     year_2016 = get_data()
-    df = pd.DataFrame(year_2016, columns = ["Sunrise", "Sunset"])
+    df = pd.DataFrame(year_2016, columns = ["Sunrise", "Sunset", "Duration of Day"])
     sunrise_y = df["Sunrise"]
     sunset_y = df['Sunset']
+    duration_y = df['Duration of Day']
     plt.plot(range(1,367), list(sunrise_y))
     plt.plot(range(1,367), list(sunset_y))
+    plt.plot(range(1,367), list(duration_y))
+    legends = ['Sunrise', 'Sunset', 'Duration of Day']
+    plt.legend(legends, loc="best")
+    plt.ylabel("minute after midnight")
+    plt.xlabel("day of year 2016")
+    plt.title("plot of sunrise and sunset times")
+    plt.xlim([0,366])
     plt.show()
+
 
 def get_data():
     time_dict = {}
@@ -35,8 +45,10 @@ def get_data():
         else:
             [sunrise, sunset] = line.strip().split(" ")
             sunrise_in_minute = int(sunrise[0:2]) * 60 + int(sunrise[2:])
+            # sunset time converted to minutes past noon
             sunset_in_minute = int(sunset[0:2]) * 60 + int(sunset[2:])
-            time_dict[month_num][day] = (sunrise_in_minute, sunset_in_minute)
+            day_light_duration = sunset_in_minute - sunrise_in_minute
+            time_dict[month_num][day] = [sunrise_in_minute, sunset_in_minute, day_light_duration]
             day += 1
     f.close()
     # think: shouldn't I save the entire year as a list of tuples of datetime?
